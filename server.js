45d46e77c60c67888e72c0760a6bc584d46b13c6
DSordia <2kdavid2k@gmail.com>
2021-01-10 15:17:12 -0600

    Updated server.js

--- server.js
+++ server.js
@@ -36,9 +36,9 @@ app.use('/api/auth', auth)
 //     })
 // }
 
-app.use(express.static(path.join(__dirname, 'build')))
+app.use(express.static(path.join(__dirname, 'frontend/build')))
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
 })
 
 const PORT = process.env.PORT || 5000
